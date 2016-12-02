4feb571921edd370309dede401d57532d252adb5
Brent Luna <brentadamluna@gmail.com>
2016-12-02 12:16:44 -0800

    initial boilerplate code withs ome backend auth complete

---
+++ frontend/pdf.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const rootHTML = document.getElementById('rootHTML');
+  ReactDOM.render(<h1>test</h1>, rootHTML);
+});
