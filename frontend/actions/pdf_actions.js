fe2c3754f34511a6db744f9eca230f582bc297a3
Brent Luna <brentadamluna@gmail.com>
2016-12-04 15:05:40 -0800

    added some extra pdf actions and added dropzone to pdf component. need to start testing giving it a file and checking its flow

--- frontend/actions/pdf_actions.js
+++ frontend/actions/pdf_actions.js
@@ -1,7 +1,9 @@
 export const PdfConstants = {
   URL_RECEIVED: 'URL_RECEIVED',
   FETCH_PDF: 'FETCH_PDF',
-  FETCH_PDFS:'FETCH_PDFS'
+  FETCH_PDFS:'FETCH_PDFS',
+  GET_PRESIGNED_URL: 'GET_PRESIGNED_URL',
+  UPLOAD_TO_S3: 'UPLOAD_TO_S3'
 }
 
 
@@ -18,6 +20,19 @@ export const fetchPdf = pdf => ({
 
 export const fetchPdfs = () => ({
   type: PdfConstants.FETCH_PDFS
+});
+
+export const fetchPresignedURL = (prefix, filename, file) => ({
+  type: PdfConstants.GET_PRESIGNED_URL,
+  prefix: prefix, 
+  filename: filename,
+  file: file
+});
+
+export const uploadToS3 = (presignedUrl, file) => ({
+  type: PdfConstants.UPLOAD_TO_S3,
+  presignedURL: presignedUrl,
+  file: file
 });
 
 
--- frontend/components/pdf/pdf.jsx
+++ frontend/components/pdf/pdf.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
+import Dropzone from 'react-dropzone';
 
-const PDF = (props) => {
 
-  return (
-    <div>
-      <h1>pdf</h1>
-    </div>
-  )
+class PDF extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onDrop = this.onDrop.bind(this);
+  }
+
+
+  handleUpload(file) {
+    debugger 
+  }
+  render(){
+    return (
+      <div>
+        <h2>Inside PDF </h2>
+        <Dropzone onDrop={this.handleUpload}>
+          <div>
+            Drag and Drop file or Click here to select file from local storage.
+          </div>
+        </Dropzone>
+      </div>
+    )
+  }
 }
-
 export default PDF;
