04a2789214f68f8dbebf0e533d7138e766de49b8
Brent Luna <brentadamluna@gmail.com>
2016-12-05 21:55:41 -0800

    fixed login and errors rendering

--- frontend/components/login/login.jsx
+++ frontend/components/login/login.jsx
@@ -38,26 +38,29 @@ class Login extends React.Component {
 
   renderErrors() {
     let lis = [];
+    console.log(this.props.errors)
+
     this.props.errors.forEach((error, i) => {
-        <li key={i}>
+      lis.push(
+        <li key={i} className='errors-li'>
           {error}
         </li>
+      );
     });
-    return (
-      <ul>
-        {lis}
-      </ul>
-    )
+    return lis;
+    
   }
 
   render() {
     return(
       <div className='login-container'>
-        <form className='login-form'>
+        <ul className='errors-ul'>
           {this.renderErrors()}
+        </ul>
+        <form className='login-form'>
           <div className='input-container'>
-            <input type='text' placeholder='username' onChange={this.update('username')} />
-            <input type='password' placeholder='password' onChange={this.update('password')} />
+            <input type='text' placeholder='username' onChange={this.update('username')} value={this.state.username}/>
+            <input type='password' placeholder='password' onChange={this.update('password')} value={this.state.password}/>
           </div>
           <div className='login-button-div'>
             <input type='submit' value='Login' onClick={this.handleLogin} />
